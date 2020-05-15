973789d1ba97f9a7bc1c99f0e5b89676811a6556
ishak.antony <ishak.antony@yahoo.com>
2020-05-15 21:55:25 +0800

    Disable tickets-with-comments Component

--- micro-services-sync/client/src/App.js
+++ micro-services-sync/client/src/App.js
@@ -3,8 +3,8 @@ import { Container, Row, Col } from "react-bootstrap";
 
 import Navbar from "./components/Navbar";
 import AddTicket from "./components/AddTicket";
-// import Tickets from "./components/Tickets";
-import Tickets from "./components/TicketsWithComments";
+import Tickets from "./components/Tickets";
+// import Tickets from "./components/TicketsWithComments";
 
 function App() {
   return (
